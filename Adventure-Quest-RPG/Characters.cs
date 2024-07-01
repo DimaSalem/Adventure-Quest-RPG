30dd3c3b29d69fa8e0a7a61e50a060487faad265
Dima Salem <demamohamad1998@gmail.com>
2024-07-01 17:19:12 +0300

    ADD Adventure Quest RPG - Part 1 Solution

---
+++ Adventure-Quest-RPG/Characters.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adventure_Quest_RPG
+{
+    public class Charachter
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackPower { get; set; }
+        public int Defense { get; set; }
+    }
+    public class PLayer: Charachter
+    {   
+        public int Level { get; set; }
+        public PLayer()
+        {
+            Name = "Player";
+            Health = 100;
+            AttackPower = 100;
+            Defense = 100;
+            Level = 1;
+        }
+    }
+    public abstract class Monster: Charachter
+    {
+        public Monster()
+        {
+            Name = "";
+            Health = 100;
+            AttackPower = 100;
+            Defense = 100;
+        }
+
+        public abstract void AttackBehavior();
+    }
+    public class Zombie : Monster
+    {
+        public Zombie()
+        {
+            Name = "Zombie";
+        }
+        public override void AttackBehavior()
+        {
+            Console.Write("The Zombie shambles forward with a guttural moan, \nswinging its rotting arms to strike its prey");
+        }
+    }
+    public class Skeleton : Monster
+    {
+        public Skeleton()
+        {
+            Name = "Skeleton";
+        }
+        public override void AttackBehavior()
+        {
+            Console.Write("The Skeleton draws its bow and fires arrows\n with deadly accuracy from a distance");
+        }
+    }
+    public class Creeper : Monster
+    {
+        public Creeper()
+        {
+            Name = "Creeper";
+        }
+        public override void AttackBehavior()
+        {
+            Console.Write("The Creeper silently approaches and hisses menacingly\n before exploding in a deadly blast");
+        }
+    }
+
+}
---
+++ AdventureQuestRPGTests/UnitTest1.cs
@@ -0,0 +1,47 @@
+using Adventure_Quest_RPG;
+
+namespace AdventureQuestRPGTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestHealthReducing_PlayerAttacksCase()
+        {
+            BattleSystem battle = new BattleSystem();
+
+            int expectedDamage = battle.player.AttackPower / 2 - battle.monster.Defense / 4;
+            int expectedHealth = battle.monster.Health - expectedDamage;
+
+            battle.Attack(battle.player, battle.monster);
+
+            int actualHealth=battle.monster.Health;
+
+            Assert.Equal(expectedHealth, actualHealth);
+        }
+
+        [Fact]
+        public void TestHealthReducing_MonsterAttacksCase()
+        {
+            BattleSystem battle = new BattleSystem();
+
+            int expectedDamage = battle.monster.AttackPower / 2 - battle.player.Defense / 4;
+            int expectedHealth = battle.player.Health - expectedDamage;
+
+            battle.Attack(battle.monster, battle.player);
+
+            int actualHealth = battle.player.Health;
+
+            Assert.Equal(expectedHealth, actualHealth);
+        }
+
+         [Fact]
+        public void TestHealthAfterBattle()
+        {
+            BattleSystem battle = new BattleSystem();
+            battle.playBattleRound();
+
+            //the winner is the player 
+            Assert.True(battle.player.Health>0);
+        }
+    }
+}
